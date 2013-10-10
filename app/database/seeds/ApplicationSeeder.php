a2474dba6fd7d0081d15b9ed2528ec7d7f3ac926
laravella <laravella.cms@gmail.com>
2013-10-10 16:26:20 +0200

    add examples to ApplicationSeeder

--- app/database/seeds/ApplicationSeeder.php
+++ app/database/seeds/ApplicationSeeder.php
@@ -1,15 +1,40 @@
-<?php class ApplicationSeeder extends Seeder {
+<?php
 
-        private function __addProduct($name, $model, $size) {
-            $product = array('name'=>$name, 'model'=>$model, 'size'=>$size);     
-            $productId = DB::table('products')->insertGetId($product);
-            Log::write('info', $name.' product added.');
-            return $productId;
-        }
+use Laravella\Crud\CrudSeeder;
+
+class ApplicationSeeder extends CrudSeeder {
+
+    public function run()
+    {
+        /**
+         * Change the title of a page (the junction between a view, a table and an action)
+         * 
+         * $tableName = an instance of the database field _db_tables.name
+         * $action = an instance of the database field _db_actions.name e.g. 'getSelect', 'getEdit' etc.
+         * $view = an instance of the database field _db_views.name e.g. 'crud::dbview'
+         * $title = the new title of the page
+         * 
+         * $this->tableActionViewId($tableName, $action, 'crud::dbview')
+         *      ->update(array('title'=>'Table Action Views'));
+         */
         
-	public function run()
-	{
-        }
+        /**
+         * Change a field label
+         * 
+         * $this->updateOrInsert('_db_fields', array('fullname'=>'contents.lang'), array('label'=>'Language'));
+         */
         
+        /**
+         * Add a new toplevel menu item
+         * 
+         * $contentId = $this->addMenu('Contents', '', 'icon-file', $topMenuId);
+         */
+        
+        /**
+         * Add a child menu item to a toplevel menu item
+         * 
+         * $this->addMenu('Pages', '/db/select/contents', 'icon-file', $contentId);
+         */
+    }
 
 }
