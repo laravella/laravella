c086f46a469f68a8b2aed74d178a2ca47c8ba167
laravella <laravella.cms@gmail.com>
2013-11-01 15:04:30 +0200

    add root route

--- app/routes.php
+++ app/routes.php
@@ -1,6 +1,5 @@
 <?php
 
-
 Route::filter('frontfilter', function($route)
 {
     if (Request::segment(1) == 'en' && Request::segment(2) == 'list')
@@ -14,6 +13,6 @@ Route::filter('frontfilter', function($route)
 Route::when('en/*', 'frontfilter'); //guest use i.e. front end
 
 Route::controller('en', 'PageController');
-
+Route::controller('/', 'PageController');
 
 ?>
